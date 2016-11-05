19d194b9b70ff9999010baee2326b2d67e2af88d
Kracav4ik <dikama2013@yandex.ru>
2016-11-05 23:00:02 +0300

    Copypasted more stuff from the orig CSS, hardcoded some standings for debug

--- web_testing/views.py
+++ web_testing/views.py
@@ -3,4 +3,10 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def my_view(request):
-    return {'word': 'and good bye'}
+    return {
+        'triesTotal': '646',
+        'triesPassed': '33',
+        'lastTeam': 'nms (guess who)',
+        'lastTask': 'A',
+        'lastTime': '1:20:03',
+    }
